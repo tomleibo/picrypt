5ae44a8e682078e1da3966e0662e1f280cc80c20
Domrev Igor <domrevigor@gmail.com>
2018-06-09 20:16:25 +0300

    adding pace js  to improve page initialization expirience

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,6 +1,17 @@
+//@flow
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+
+it('flow is super cool', () => {
+  const da = (x: number, y: number): number => x+y
+
+  var foo = {da:"boo",bar:""}
+  foo.bar = "1"
+
+  expect(da(1,2)).toBe(3) 
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
